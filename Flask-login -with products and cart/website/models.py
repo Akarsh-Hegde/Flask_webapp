d328ca0bdd311639098cd61ed7bd90df13087366
Akarsh-Hegde <hegdeakarsh2002@gmail.com>
2023-03-01 02:21:24 +0530

    Purchase(add to cart) button

--- Flask-login -with products and cart/website/models.py
+++ Flask-login -with products and cart/website/models.py
@@ -16,3 +16,10 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12),nullable=False,unique =True)
     description = db.Column(db.String(length=1024),nullable=False,unique =True)
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12),nullable=False,unique =True)
+    description = db.Column(db.String(length=1024),nullable=False,unique =True)
--- Flask-login -with products and cart/website/views.py
+++ Flask-login -with products and cart/website/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from . import db
-from .models import Item, User
+from .models import Item, User, Cart
 
 views = Blueprint('views', __name__)
 
@@ -15,6 +15,27 @@ def home():
 
 @views.route('/market', methods=['GET','POST'])
 def market_page():
-    
+
+    if request.method == "POST":
+
+        name = request.form.get('name')   
+        barcode = request.form.get('barcode')
+        price = request.form.get('price')
+        description = request.form.get('description')
+        
+        new_cart_item = Cart(name=name, barcode=barcode, price=price, description=description)
+        print(new_cart_item)
+        db.session.add(new_cart_item)
+        db.session.commit()
+        flash('Item Purchased!', category='success')
+        return redirect(url_for("views.cart_page"))
+
     items = Item.query.all()
     return render_template('market.html', user=current_user, items=items)
+
+
+@views.route('/cart', methods=['GET','POST'])
+def cart_page():
+
+    carts = Cart.query.all()
+    return render_template('cart.html', user=current_user, carts=carts)
