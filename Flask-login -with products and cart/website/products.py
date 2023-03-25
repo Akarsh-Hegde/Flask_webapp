37fa346db7d63248059da9f2377ecd462b2eb5eb
Akarsh-Hegde <hegdeakarsh2002@gmail.com>
2023-03-25 17:01:32 +0530

    new changes

--- Flask-login -with products and cart/website/products.py
+++ Flask-login -with products and cart/website/products.py
@@ -6,7 +6,6 @@ from flask_login import login_required, current_user
 
 products = Blueprint('products', __name__)
 
-
 @products.route('/admin', methods=['GET', 'POST'])
 @login_required
 def admin_page():
@@ -16,18 +15,49 @@ def admin_page():
         price = request.form.get('price')
         quantity = request.form.get('quantity')
         description = request.form.get('description')
+        
+        id = request.form.get('id')   
+        item_check = Item.query.filter_by(id=id).first()
+        
+        if request.form.get('add') == 'Add':
+            if item_check:
+                flash("Product already exists", category="error")
+            else:  
+                new_item = Item(name=name, barcode=barcode, price=price, quantity=quantity, description=description)
+                db.session.add(new_item)
+                db.session.commit()
+                flash('Item Added!', category='success')
+                return redirect(url_for("products.admin_page"))
+        
+        id = request.form.get('id')   
+        item = Item.query.filter_by(id=id).first()
 
-        item = Item.query.filter_by(name=name).first()
-        if item:
-            flash("Product already exists", category="error")
-        else:
-            new_item = Item(name=name, barcode=barcode, price=price, quantity=quantity, description=description)
-            db.session.add(new_item)
+        if request.form.get('remove') == 'Remove':
+            db.session.delete(item)
             db.session.commit()
-            flash('Item Added!', category='success')
-            return redirect(url_for("views.home"))
-    carts = Cart.query.all()
+            flash('Item Removed!', category='success')
+            return redirect(url_for("products.admin_page"))
+
+        if  request.form.get('edit') == 'Edit':
+            # db.session.delete(item)
+            # db.session.commit()
+            if item:
+                name = request.form.get('name')
+                barcode = request.form.get('barcode')
+                price = request.form.get('price')
+                quantity = request.form.get('quantity')
+                description = request.form.get('description')
+            
+                item1 = Item(name=name, barcode=barcode, price=price, quantity=quantity, description=description)
+                db.session.add(item1)
+                db.session.commit()
+                flash('Item Updated(delete+add=edit)!', category='success')
+                return redirect(url_for("products.admin_page"))
+            else:
+                flash("Product doesnt exists", category="error")
+
     items = Item.query.all()
+    carts = Cart.query.all()
 
     return render_template("admin.html", user=current_user, carts=carts, items=items)
 
